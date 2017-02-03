9a4515837f833d9cede651573f97c0bbda838f2e
Harris Brakmic <brakmic@web.de>
2017-02-03 09:53:38 +0100

    reorder method exports in Address Calls

--- src/api/v1/calls/addresses.ts
+++ src/api/v1/calls/addresses.ts
@@ -3,21 +3,23 @@ import { Address, AddressResponse } from '../types';
 import * as _ from 'lodash';
 const protocol = 'https';
 const addressesUrl = `${protocol}://${API_URLS['addresses'].server}/${API_URLS['addresses'].version}/addresses`;
-const readAll = (authHeader: any): Promise<Address[]> => {
-    return RequestHelper.get(addressesUrl, authHeader);;
-};
-
-const read = (authHeader: any, id: string): Promise<Address> => {
-    return RequestHelper.get(`${addressesUrl}/${id}`, authHeader);
-};
 
 const create = (authHeader: any, address: Address): Promise<AddressResponse> => {
     return RequestHelper.post(addressesUrl, address, authHeader);
 };
 
+const read = (authHeader: any, id: string): Promise<AddressResponse> => {
+    return RequestHelper.get(`${addressesUrl}/${id}`, authHeader);
+};
+
+const readAll = (authHeader: any): Promise<AddressResponse[]> => {
+    return RequestHelper.get(addressesUrl, authHeader);;
+};
+
+
 export const Addresses = {
+    create,
     read,
-    readAll,
-    create
+    readAll
 };
 
