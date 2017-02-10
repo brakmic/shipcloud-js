aa903c396d3adb63d2c6140d5ab8607bfc6cac61
Harris Brakmic <brakmic@web.de>
2017-02-10 11:41:49 +0100

    url

--- src/api/v1/calls/carriers.ts
+++ src/api/v1/calls/carriers.ts
@@ -2,10 +2,10 @@ import { RequestHelper, API_URLS } from '../../base';
 import { CarrierResponse } from '../types';
 import * as _ from 'lodash';
 const protocol = 'https';
-const addressesUrl = `${protocol}://${API_URLS['carriers'].server}/${API_URLS['carriers'].version}/addresses`;
+const carriersUrl = `${protocol}://${API_URLS['carriers'].server}/${API_URLS['carriers'].version}/addresses`;
 
 const readAll = (authHeader: any): Promise<CarrierResponse[]> => {
-    return RequestHelper.get(addressesUrl, authHeader);;
+    return RequestHelper.get(carriersUrl, authHeader);;
 };
 
 export const Carriers = {
