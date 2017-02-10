fde0d87d892d83b9eb84c834c5295943eaa92a29
Harris Brakmic <brakmic@web.de>
2017-02-10 11:43:54 +0100

    wrong url in carriers

--- src/api/v1/calls/carriers.ts
+++ src/api/v1/calls/carriers.ts
@@ -2,7 +2,7 @@ import { RequestHelper, API_URLS } from '../../base';
 import { CarrierResponse } from '../types';
 import * as _ from 'lodash';
 const protocol = 'https';
-const carriersUrl = `${protocol}://${API_URLS['carriers'].server}/${API_URLS['carriers'].version}/addresses`;
+const carriersUrl = `${protocol}://${API_URLS['carriers'].server}/${API_URLS['carriers'].version}/carriers`;
 
 const readAll = (authHeader: any): Promise<CarrierResponse[]> => {
     return RequestHelper.get(carriersUrl, authHeader);;
